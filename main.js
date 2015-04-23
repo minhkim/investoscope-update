2fe23c5dff7b37dd8155a45c7631f5d281badd90
Thorbjørn Hermansen <thhermansen@gmail.com>
2015-04-23 21:21:06 +0200

    Investoscope is now being updated with data from Netfonds.

--- main.js
+++ main.js
@@ -5,12 +5,16 @@ import * as netfonds from "./lib/netfonds";
 var debug = debugGenerator("investoscope.main");
 
 export async function run(options) {
-  debug("Fetching quotes.. ");
+  debug("Fetching quotes..");
 
-  var quotes = await investoscope.getQuotes();
-  var result = await netfonds.getAll(quotes.slice(0, 1));
+  try {
+    var quotes = await investoscope.getQuotes();
+    var endOfDayQuotes = await netfonds.getAll(quotes.slice(0, 1));
+    await investoscope.update(endOfDayQuotes);
 
-  debug(result);
-
-  debug("DONE fetching quotes.. ");
+  } catch (err) {
+    debug(err)
+  } finally {
+    debug("DONE fetching quotes..");
+  }
 }
